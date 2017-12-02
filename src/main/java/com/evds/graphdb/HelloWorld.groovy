cba3ca12a190bba7d8cee5e08147692d73a6c46d
pmmanoj <pmanoj@ibdsys.com>
2017-12-02 04:56:54 -0600

    First Groovy compiled class

---
+++ src/main/java/com/evds/graphdb/HelloWorld.groovy
@@ -0,0 +1,9 @@
+package com.evds.graphdb
+
+/**
+ * Created by sanjivsingh on 12/2/17.
+ */
+
+println "Hello, World -- Groovy"
+println "Using Maven via Artifactory and reaching Maven Central"
+println "Bye"
